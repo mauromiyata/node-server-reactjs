939369b119f6871418e087a8816a49cbd2c7c439
Mauro Toshio Miyata <mauro_miyata@yahoo.com.br>
2020-02-12 09:30:50 -0300

    add header control allow origin

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const STATIC_PATH = path.resolve(__dirname, process.env.STATIC_PATH);
 
 app.use(express.static(STATIC_PATH));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('*', (req, res) => {
   res.sendFile(STATIC_PATH + path.sep + 'index.html');
 });
